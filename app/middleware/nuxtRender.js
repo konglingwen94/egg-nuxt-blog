639c3116e7b835210c941a6961b4a2930aea5211
konglingwen94 <794430774@qq.com>
2019-09-30 22:04:37 +0800

    chore config files

--- app/middleware/nuxtRender.js
+++ app/middleware/nuxtRender.js
@@ -2,7 +2,11 @@ const { Nuxt } = require('nuxt')
 
 module.exports = (option, app) => {
   const { nuxtConfig } = option
+
+  console.log(nuxtConfig)
+
   const nuxt = new Nuxt(nuxtConfig)
+
   return async ctx => {
     ctx.status = 200
     ctx.respond = false
