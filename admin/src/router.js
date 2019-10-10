f82662416e59a656d30e154ad76d51defe8fe67e
konglingwen94 <794430774@qq.com>
2019-10-10 18:26:09 +0800

    Refactory

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,20 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import BasicLayout from '@/layouts/Basic'
-import UserLayout from '@/layouts/User'
-import NotFound from '@/layouts/NotFound'
+const BasicLayout = import('@/layouts/Basic')
+const UserLayout = import('@/layouts/User')
+const NotFound = import('@/layouts/NotFound')
 
-import Dashboard from '@/views/Dashboard'
-import Login from '@/views/Login'
-import Security from '@/views/Security'
-import ArticleList from '@/views/Articles/ArticleList.vue'
-import ArticleEditor from '@/views/Articles/ArticleEditor'
-import Categories from '@/views/Categories/List'
-import Comment from '@/views/Comments/List'
-import Guestbook from '@/views/Guestbooks/List'
-import About from '@/views/About'
-import Tag from '@/views/Tags'
+const Dashboard = import('@/views/Dashboard')
+const Login = import('@/views/Login')
+const Security = import('@/views/Security')
+const ArticleList = import('@/views/Articles/List.vue')
+const ArticleEditor = import('@/views/Articles/Editor.vue')
+const Category = import('@/views/Categories/List')
+const Comment = import('@/views/Comments/List')
+const Guestbook = import('@/views/Guestbooks/List')
+const About = import('@/views/About')
+const Tag = import('@/views/Tags')
+
+// import BasicLayout from '@/layouts/Basic'
+// import UserLayout from '@/layouts/User'
+// import NotFound from '@/layouts/NotFound'
+
+// import Dashboard from '@/views/Dashboard'
+// import Login from '@/views/Login'
+// import Security from '@/views/Security'
+// import ArticleList from '@/views/Articles/ArticleList.vue'
+// import ArticleEditor from '@/views/Articles/ArticleEditor'
+// import Categories from '@/views/Categories/List'
+// import Comment from '@/views/Comments/List'
+// import Guestbook from '@/views/Guestbooks/List'
+// import About from '@/views/About'
+// import Tag from '@/views/Tags'
 
 Vue.use(Router)
 
@@ -98,7 +113,7 @@ const router = new Router({
         {
           name: 'categories',
           path: 'categories',
-          component: Categories,
+          component: Category,
           meta: {
             title: '文章分类列表',
             activedMenuItem: 'categories',
