318c123ec1d1aa60ad05ab63cfdff626745e4538
konglingwen94 <794430774@qq.com>
2019-10-10 16:55:13 +0800

    Improve docs

--- app/middleware/docs.js
+++ app/middleware/docs.js
@@ -2,15 +2,24 @@ const { historyApiFallback } = require('koa2-connect-history-api-fallback')
 
 module.exports = option => {
   return historyApiFallback({
+    // verbose: true,
+    // index: '/docs/index.html',
     rewrites: [
       {
-        from: /[^\/docs\/assets\/.*]\/.*$/,
+        from: /\/docs/,
         to: function(context) {
-          console.log(context.parsedUrl.pathname)
+          const reg = /\/docs\/assets\//
+          console.log(
+            context.parsedUrl.pathname,
+            context.parsedUrl.pathname.match(reg)
+          )
+          if (context.parsedUrl.pathname.match(reg)) {
+            return context.parsedUrl.pathname
+          }
           if (context.parsedUrl.pathname.endsWith('.html')) {
             return context.parsedUrl.pathname
           }
-          return context.parsedUrl.pathname + 'index.html'
+          return context.parsedUrl.pathname + '/index.html'
         },
       },
     ],
