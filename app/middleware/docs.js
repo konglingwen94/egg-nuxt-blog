b638c9f7d96f77ec28fefa5fffff945e73c636a9
konglingwen94 <794430774@qq.com>
2019-10-10 15:59:21 +0800

    bug fix

--- app/middleware/docs.js
+++ app/middleware/docs.js
@@ -2,6 +2,18 @@ const { historyApiFallback } = require('koa2-connect-history-api-fallback')
 
 module.exports = option => {
   return historyApiFallback({
-    index: '/docs/index.html',
+    
+    rewrites: [
+      {
+        from: /[^\/docs\/assets]\/.*$/,
+        to: function(context) {
+          console.log(context.parsedUrl.pathname)
+          if (context.parsedUrl.pathname.endsWith('.html')) {
+            return context.parsedUrl.pathname
+          }
+          return context.parsedUrl.pathname + 'index.html'
+        },
+      },
+    ],
   })
 }
