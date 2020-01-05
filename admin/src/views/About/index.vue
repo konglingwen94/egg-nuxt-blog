711e9eb067b9e40d1035b4541f8855b1ae9051ad
konglingwen94 <794430774@qq.com>
2020-01-05 17:08:02 +0800

    Improve

--- admin/src/views/About/index.vue
+++ admin/src/views/About/index.vue
@@ -1,30 +1,30 @@
 <template>
   <panel class="about">
-    <div v-if="state.id" slot="header">
+    <div v-if="id" slot="header">
       <el-button type="primary" @click="resetConfig">重置</el-button>
     </div>
 
     <div class="carousel-wrapper">
       <el-card header="轮播图配置">
-        <el-form :model="state.carousel">
+        <el-form :model="carousel">
           <el-form-item label="轮播图个数">
-            <el-input-number :min="1" v-model.number="state.carousel.number"></el-input-number>
+            <el-input-number :min="1" v-model.number="carousel.number"></el-input-number>
           </el-form-item>
 
           <el-form-item label="轮播图排序">
-            <el-radio-group v-model="state.carousel.sort">
+            <el-radio-group v-model="carousel.sort">
               <el-radio label="pv">浏览量</el-radio>
               <el-radio label="starCount">收藏个数</el-radio>
             </el-radio-group>
           </el-form-item>
           <el-form-item label="自动切换时间">
-            <el-input-number :min="500" :step="500" v-model="state.carousel.interval"></el-input-number>
+            <el-input-number :min="500" :step="500" v-model="carousel.interval"></el-input-number>
           </el-form-item>
           <el-form-item label="是否自动播放">
-            <el-switch v-model="state.carousel.autoplay"></el-switch>
+            <el-switch v-model="carousel.autoplay"></el-switch>
           </el-form-item>
           <el-form-item label="是否循环播放">
-            <el-switch v-model="state.carousel.loop"></el-switch>
+            <el-switch v-model="carousel.loop"></el-switch>
           </el-form-item>
           <el-form-item>
             <el-button @click="submit('carousel')" type="primary">保存</el-button>
@@ -36,7 +36,7 @@
       <el-card header="博主简介">
         <el-form>
           <el-form-item>
-            <el-input type="textarea" clearable :rows="4" v-model="state.profile.description"></el-input>
+            <el-input type="textarea" clearable :rows="4" v-model="profile.description"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button @click="submit('profile')" type="primary">保存</el-button>
@@ -48,7 +48,7 @@
       <el-card header="博客简介">
         <el-form>
           <el-form-item>
-            <el-input type="textarea" clearable :rows="4" v-model="state.platform.description"></el-input>
+            <el-input type="textarea" clearable :rows="4" v-model="platform.description"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button @click="submit('platform')" type="primary">保存</el-button>
@@ -70,27 +70,26 @@ const mapStateKey = {
 export default {
   name: 'About',
   data: () => ({
-    state: {
-      profile: {
-        description: ''
-      },
-      platform: {
-        description: ''
-      },
-      carousel: {
-        // number: -1,
-        // sort: '',
-        // autoplay: false,
-        // loop: false,
-        // interval: -1
-      }
+    profile: {
+      // description: ''
+    },
+    platform: {
+      // description: ''
+    },
+    id:'',
+    carousel: {
+      // number: -1,
+      // sort: '',
+      // autoplay: false,
+      // loop: false,
+      // interval: -1
     }
   }),
   created() {
     AboutApi.fetchOne().then(response => {
-      for (let key in response) {
-        this.$set(this.state, key, response[key])
-      }
+
+      _.assign(this.$data,response)
+      
     })
   },
   methods: {
@@ -103,27 +102,26 @@ export default {
         return
       }
 
-      AboutApi.resetOne(this.state.id)
+      AboutApi.resetOne(this.id)
         .then(result => {
           this.$message.success('重置配置成功')
           // invokeDeepObject(this.state)
-          _.merge(this.state, result)
+          _.merge(this.$data, result)
         })
         .catch(err => {
           this.$message.error(err.message)
         })
     },
     submit(key) {
-      const { id } = this.state
-      const payload = { [key]: this.state[key] }
+      const { id } = this
+      const payload = { [key]: this.$data[key] }
       const action = id
         ? AboutApi.updateOne(id, payload)
         : AboutApi.createOne(payload)
       action
         .then(response => {
-          if (id) {
-          } else {
-            this.$set(this.state, 'id', response.id)
+          if (response.id) {
+            this.id = response.id
           }
           this.$message.success(`设置${mapStateKey[key]}成功`)
         })
