dde63d4757a26027a044578b803e2975a5802654
konglingwen94 <794430774@qq.com>
2020-02-26 21:46:25 +0800

    feat:(server) remove upload api

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -1,5 +1,17 @@
+const path=require('path')
+const fs=require('fs')
 const { Controller } = require('egg')
 module.exports = class UploadController extends Controller {
+
+  async deleteFile(){
+    const {ctx}=this
+    const {baseDir,upload}=this.app.config
+
+    console.log(path.resolve(baseDir,upload.dir))
+    console.log(this.app.config)
+    require('fs').unlinkSync(path.resolve(baseDir,upload.dir,ctx.params.filename))
+    return path.resolve(baseDir,upload.dir)
+  }
   async uploadFile() {
     const { ctx } = this
     const name = ctx.req.file.filename
--- app/router/upload.js
+++ app/router/upload.js
@@ -2,4 +2,6 @@ module.exports = app => {
   const {proxyRouter,controller} = app
 
   proxyRouter.post('/upload', controller.upload.uploadFile)
+
+  proxyRouter.delete('/uploads/:filename',controller.upload.deleteFile)
 }
