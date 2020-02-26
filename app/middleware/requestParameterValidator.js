69e0b27f0b13bb6fea70cb7d3c3c677ed303457f
konglingwen94 <794430774@qq.com>
2020-02-26 19:24:53 +0800

    Refactory parameter validator  middleware

--- app/middleware/requestParameterValidator.js
+++ app/middleware/requestParameterValidator.js
@@ -1,43 +1,23 @@
 const _ = require('lodash')
-const defaultValidationRule = require('../types/request')
+const validationRule = require('../types/request')
 
 module.exports = (opt, app) => {
-  const patchValidationRule = _.cloneDeep(defaultValidationRule)
-  for (let routerName in patchValidationRule) {
-    if (routerName === 'configuration') 
-    continue
-    for (let field in patchValidationRule[routerName]) {
-      patchValidationRule[routerName][field].required = false
-    }
-  }
+ 
 
-  
   return async function(ctx, next) {
-    const matchMethods = ['POST', 'PATCH']
+    const matchRule = validationRule[ctx.method]
 
-    if (!matchMethods.includes(ctx.method) || !ctx.routerName) {
+    if (!matchRule || !ctx.routerName) {
       return next()
     }
 
-    switch (ctx.method) {
-      case 'PATCH':
-        ctx.validate(
-          _.get(patchValidationRule, ctx.routerName),
-          ctx.request.body
-        )
-        break
-      case 'POST':
-        ctx.validate(
-          _.get(defaultValidationRule, ctx.routerName),
-          ctx.request.body
-        )
-    }
+    ctx.validate(_.get(matchRule, ctx.routerName), ctx.request.body)
 
     ctx.state.body = _.pick(
       ctx.request.body,
-      _.keys(_.get(defaultValidationRule, ctx.routerName))
+      _.keys(_.get(matchRule, ctx.routerName))
     )
-
+ 
     return next()
   }
 }
--- app/types/request.js
+++ app/types/request.js
@@ -1,200 +1,403 @@
 module.exports = {
-  configuration: {
-    profile: {
-      personal: {
-        type: 'object?',
-        rule: {
-          contact: {
-            type: 'array',
-            itemType: 'object',
-            rule: {
-              label: {
-                type: 'string',
-                value: 'string',
+  POST: {
+    configuration: {
+      profile: {
+        personal: {
+          type: 'object?',
+          rule: {
+            contact: {
+              type: 'array',
+              itemType: 'object',
+              rule: {
+                label: {
+                  type: 'string',
+                  value: 'string',
+                },
               },
+              min: 1,
             },
+            description: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+        technology: {
+          type: 'object?',
+          rule: {
+            frontend: {
+              type: 'string',
+              min: 1,
+            },
+            serverSide: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+      },
+      projectIntro: {
+        platform: {
+          type: 'object?',
+          rule: {
+            webClient: {
+              type: 'string',
+              min: 1,
+            },
+            serverUI: {
+              type: 'string',
+              min: 1,
+            },
+            serverApi: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+      },
+      siteConfig: {
+        carousel: {
+          type: 'object?',
+          rule: {
+            number: {
+              type: 'int',
+              min: 1,
+            },
+            sort: {
+              type: 'enum',
+              values: ['pv', 'starCount'],
+            },
+            autoplay: 'boolean',
+            loop: 'boolean',
+            interval: {
+              type: 'int',
+              min: 500,
+            },
+          },
+        },
+        message: {
+          type: 'object?',
+          rule: {
+            renderLayer: 'int',
+            expandLayer: 'int',
+          },
+        },
+      },
+    },
+
+    account: {
+      username: {
+        type: 'string',
+        min: 1,
+      },
+      password: {
+        type: 'string',
+        min: 6,
+      },
+      nickname: {
+        type: 'string?',
+        min: 1,
+      },
+      role: {
+        type: 'enum?',
+        values: ['JUNIOR', 'SENIOR'],
+      },
+      level: {
+        type: 'enum?',
+        values: [1, 10],
+      },
+    },
+    article: {
+      categoryID: 'objectId',
+      tagIdList: 'objectIdList',
+      title: {
+        type: 'string',
+        min: 1,
+      },
+      content: {
+        type: 'object',
+
+        rule: {
+          html: {
+            type: 'string',
+          },
+          text: {
+            type: 'string',
+          },
+        },
+      },
+      cover: {
+        type: 'object?',
+
+        rule: {
+          name: {
+            type: 'string?',
             min: 1,
           },
-          description: {
-            type: 'string',
+          path: {
+            type: 'string?',
             min: 1,
           },
         },
       },
-      technology: {
+
+      isPublished: {
+        type: 'boolean?',
+        default: true,
+      },
+    },
+    category: {
+      name: {
+        type: 'string',
+        min: 1,
+      },
+      cover: {
+        type: 'object',
+        rule: {
+          path: 'string',
+          name: 'string',
+        },
+      },
+    },
+    comment: {
+      content: {
+        type: 'string',
+        min: 1,
+      },
+      email: {
+        type: 'email',
+      },
+      nickname: {
+        type: 'string',
+        min: 1,
+      },
+    },
+
+    message: {
+      content: {
+        type: 'string',
+        min: 1,
+      },
+      email: {
+        type: 'email',
+      },
+      nickname: {
+        type: 'string',
+        min: 1,
+      },
+    },
+    tag: {
+      name: {
+        type: 'string',
+        min: 1,
+      },
+      cover: {
+        type: 'object',
+        rule: {
+          path: 'string',
+          name: 'string',
+        },
+      },
+    },
+  },
+  PATCH: {
+    configuration: {
+      profile: {
+        personal: {
+          type: 'object?',
+          rule: {
+            contact: {
+              type: 'array',
+              itemType: 'object',
+              rule: {
+                label: {
+                  type: 'string',
+                  value: 'string',
+                },
+              },
+              min: 1,
+            },
+            description: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+        technology: {
+          type: 'object?',
+          rule: {
+            frontend: {
+              type: 'string',
+              min: 1,
+            },
+            serverSide: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+      },
+      projectIntro: {
+        platform: {
+          type: 'object?',
+          rule: {
+            webClient: {
+              type: 'string',
+              min: 1,
+            },
+            serverUI: {
+              type: 'string',
+              min: 1,
+            },
+            serverApi: {
+              type: 'string',
+              min: 1,
+            },
+          },
+        },
+      },
+      siteConfig: {
+        carousel: {
+          type: 'object?',
+          rule: {
+            number: {
+              type: 'int',
+              min: 1,
+            },
+            sort: {
+              type: 'enum',
+              values: ['pv', 'starCount'],
+            },
+            autoplay: 'boolean',
+            loop: 'boolean',
+            interval: {
+              type: 'int',
+              min: 500,
+            },
+          },
+        },
+        message: {
+          type: 'object?',
+          rule: {
+            renderLayer: 'int',
+            expandLayer: 'int',
+          },
+        },
+      },
+    },
+
+    account: {
+      username: {
+        type: 'string',
+        min: 1,
+      },
+      password: {
+        type: 'string',
+        min: 6,
+      },
+      nickname: {
+        type: 'string?',
+        min: 1,
+      },
+      role: {
+        type: 'enum?',
+        values: ['JUNIOR', 'SENIOR'],
+      },
+      level: {
+        type: 'enum?',
+        values: [1, 10],
+      },
+    },
+    article: {
+      categoryID: 'objectId?',
+      tagIdList: 'objectIdList?',
+      title: {
+        type: 'string?',
+        min: 1,
+      },
+      content: {
+        type: 'object?',
+
+        rule: {
+          html: {
+            type: 'string',
+          },
+          text: {
+            type: 'string',
+          },
+        },
+      },
+      cover: {
+        type: 'object?',
+
+        rule: {
+          name: {
+            type: 'string?',
+            min: 1,
+          },
+          path: {
+            type: 'string?',
+            min: 1,
+          },
+        },
+      },
+
+      isPublished: {
+        type: 'boolean?',
+         
+      },
+    },
+    category: {
+      name: {
+        type: 'string?',
+        min: 1,
+      },
+      cover: {
         type: 'object?',
         rule: {
-          frontend: {
-            type: 'string',
-            min: 1,
-          },
-          serverSide: {
-            type: 'string',
-            min: 1,
-          },
+          path: 'string',
+          name: 'string',
         },
       },
     },
-    projectIntro: {
-      platform: {
+    comment: {
+      content: {
+        type: 'string?',
+        min: 1,
+      },
+      email: {
+        type: 'email?',
+      },
+      nickname: {
+        type: 'string?',
+        min: 1,
+      },
+    },
+
+    message: {
+      content: {
+        type: 'string',
+        min: 1,
+      },
+      email: {
+        type: 'email',
+      },
+      nickname: {
+        type: 'string',
+        min: 1,
+      },
+    },
+    tag: {
+      name: {
+        type: 'string?',
+        min: 1,
+      },
+      cover: {
         type: 'object?',
         rule: {
-          webClient: {
-            type: 'string',
-            min: 1,
-          },
-          serverUI: {
-            type: 'string',
-            min: 1,
-          },
-          serverApi: {
-            type: 'string',
-            min: 1,
-          },
+          path: 'string',
+          name: 'string',
         },
-      },
-    },
-    siteConfig: {
-      carousel: {
-        type: 'object?',
-        rule: {
-          number: {
-            type: 'int',
-            min: 1,
-          },
-          sort: {
-            type: 'enum',
-            values: ['pv', 'starCount'],
-          },
-          autoplay: 'boolean',
-          loop: 'boolean',
-          interval: {
-            type: 'int',
-            min: 500,
-          },
-        },
-      },
-      message: {
-        type: 'object?',
-        rule: {
-          renderLayer: 'int',
-          expandLayer: 'int',
-        },
-      },
-    },
-  },
-
-  account: {
-    username: {
-      type: 'string',
-      min: 1,
-    },
-    password: {
-      type: 'string',
-      min: 6,
-    },
-    nickname: {
-      type: 'string?',
-      min: 1,
-    },
-    role: {
-      type: 'enum?',
-      values: ['JUNIOR', 'SENIOR'],
-    },
-    level: {
-      type: 'enum?',
-      values: [1, 10],
-    },
-  },
-  article: {
-    categoryID: 'objectId',
-    tagIdList: 'objectIdList',
-    title: {
-      type: 'string',
-      min: 1,
-    },
-    content: {
-      type: 'object',
-
-      rule: {
-        html: {
-          type: 'string',
-        },
-        text: {
-          type: 'string',
-        },
-      },
-    },
-    cover: {
-      type: 'object?',
-
-      rule: {
-        name: {
-          type: 'string?',
-          min: 1,
-        },
-        path: {
-          type: 'string?',
-          min: 1,
-        },
-      },
-    },
-
-    isPublished: {
-      type: 'boolean?',
-      default: true,
-    },
-  },
-  category: {
-    name: {
-      type: 'string',
-      min: 1,
-    },
-    cover: {
-      type: 'object',
-      rule: {
-        path: 'string',
-        name: 'string',
-      },
-    },
-  },
-  comment: {
-    content: {
-      type: 'string',
-      min: 1,
-    },
-    email: {
-      type: 'email',
-    },
-    nickname: {
-      type: 'string',
-      min: 1,
-    },
-  },
-
-  message: {
-    content: {
-      type: 'string',
-      min: 1,
-    },
-    email: {
-      type: 'email',
-    },
-    nickname: {
-      type: 'string',
-      min: 1,
-    },
-  },
-  tag: {
-    name: {
-      type: 'string',
-      min: 1,
-    },
-    cover: {
-      type: 'object',
-      rule: {
-        path: 'string',
-        name: 'string',
       },
     },
   },
