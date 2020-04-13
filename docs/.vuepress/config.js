16f4733e4da1f785aab2feda66678431e9c49b4f
konglingwen94 <794430774@qq.com>
2020-04-13 11:45:29 +0800

    Build docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,9 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const config = {
-  dest: path.resolve(__dirname, '../../public/docs'),
-  base: '/egg-nuxt-blog/',
+  // dest: path.resolve(__dirname, '../../public/docs'),
+  
+  base: '/egg-nuxt-blog/docs/dist',
   title: '博客',
   themeConfig: {
      
